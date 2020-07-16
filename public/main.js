6b76dae745a42fd9a2f29edb6b5135bb363e3580
Mikhail Grischenko <thomson.mikhail@gmail.com>
2020-07-16 09:07:03 +0300

    fix complete

--- public/main.js
+++ public/main.js
@@ -18,56 +18,30 @@ recognition.lang = 'en-US';
 recognition.interimResults = false;
 recognition.maxAlternatives = 1;
 
-const microphoneIcon = document.querySelector('.microphone__image');
-const microphoneWrapper = document.querySelector('.microphone-wrapper');
-const audioRecordAnimation = document.querySelector('.audio-record-animation');
-const speechRecognitionSection = document.querySelector(
-  '.speech-recognition-section'
-);
-const recognitionTextResult = document.querySelector('.recognition-result');
+const recognitionTextResultHi = document.querySelector('.recognition-result--hi');
+const recognitionTextResultBye = document.querySelector('.recognition-result--bye');
+
+let hi = [];
+let bye = [];
 
 function checkResult(speechResult) {
   return {
-    received: !!speechResult.length,
-    correctResult: !!speechResult.length ? Object.values(words).includes(speechResult[0][0].transcript) : false,
-    result: speechResult[0][0].transcript,
+    bye: speechResult.transcript.toLocaleLowerCase().match(/bye/g),
+    hi: speechResult.transcript.toLocaleLowerCase().match(/hi/g),
   }
 }
 
-microphoneIcon.onclick = function() {
-  recognition.start();
-  console.log('Waiting for a voice command');
-};
-
-recognition.onaudiostart = function() {
-  microphoneWrapper.style.visibility = 'hidden';
-  audioRecordAnimation.style.visibility = 'visible';
-};
-
 recognition.onresult = function(event) {
-  const result = checkResult(event.results);
-  if (!result.received) {
-    recognitionTextResult.textContent = `К сожалению ничего не услышали. Попробуйте еще раз`;
-  } else {
-    if (result.correctResult) {
-      recognitionTextResult.textContent = `Правильный результат: ${result.result}`;
-    } else {
-      recognitionTextResult.textContent = `Ожидали "hi" или "bye", а услышали: "${result.result}"`;
-    }
-  }
+  const result = checkResult(event.results[0][0]);
+  if (result.hi) hi = hi.concat(result.hi)
+  if (result.bye) bye = bye.concat(result.bye)
+  recognitionTextResultHi.textContent = hi.join(', ');
+  recognitionTextResultBye.textContent = bye.join(', ');
   console.log('Confidence: ' + event.results[0][0].confidence);
 };
 
-recognition.onspeechend = function() {
-  recognition.stop();
-  microphoneWrapper.style.visibility = 'visible';
-  audioRecordAnimation.style.visibility = 'hidden';
+recognition.onend = function() {
+  recognition.start();
 };
 
-recognition.onnomatch = function(event) {
-  alert("I didn't recognise that color.");
-};
-
-recognition.onerror = function(event) {
-  alert(`Error occurred in recognition: ${event.error}`);
-};
+recognition.start();
